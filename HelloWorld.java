c4d0ab49217cb88b0e79eca47543cc06fa8b0093
ariyonbell0-debug <ariyonbell0@gmail.com>
2026-08-27 17:11:50 -0400

    Revert "Update HelloWorld.java"
    This reverts commit 3bc4c00177c513fd230163feb5fe4cb5fbd188dd.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,2 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
-    }
-}
+# -hello-world
+hello-world practice repository. 4
