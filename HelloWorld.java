3bc4c00177c513fd230163feb5fe4cb5fbd188dd
ariyonbell0-debug <ariyonbell0@gmail.com>
2026-08-27 17:10:06 -0400

    Update HelloWorld.java
    dded a second print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,2 +1,6 @@
-# -hello-world
-hello-world practice repository. 4
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
+    }
+}
